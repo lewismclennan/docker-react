3ad8fa93d41c15d7faf92bdda9018d3d1fcd9e89
Lewis McLennan <lewismclennan@pop-os.localdomain>
2020-04-28 13:33:00 +0100

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hi Lewis!/i);
+  const linkElement = getByText(/Hi Lewis, I was changed on the feature branch!/i);
   expect(linkElement).toBeInTheDocument();
 });
